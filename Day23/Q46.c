e3a684b84100ab83dd7bf74449ab193a24bd56d4
snehaydv1090-coder <snehaydv1090@gmail.com>
2026-09-21 14:28:11 +0530

    Day23-Q46

---
+++ Day23/Q46.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int row, column;
+
+    for (row = 1; row <= 5; row++)
+    {
+        for (column = 1; column <= 5; column++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+}
