da8c16518fceee12a375ad7c0fdfb9c01b4ff1a4
snehaydv1090-coder <snehaydv1090@gmail.com>
2026-09-21 12:26:01 +0530

    Day04-Q7
    This program swaps two numbers without using a temporary variable.

---
+++ Day 04/Q7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int number1, number2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &number1, &number2);
+
+    printf("Before swapping: number1 = %d, number2 = %d\n",
+           number1, number2);
+
+    number1 = number1 + number2;
+    number2 = number1 - number2;
+    number1 = number1 - number2;
+
+    printf("After swapping: number1 = %d, number2 = %d\n",
+           number1, number2);
+}
