26faf30c9612aa0a0f40bb2976553363401aa076
snehaydv1090-coder <snehaydv1090@gmail.com>
2026-09-21 12:21:12 +0530

    Day03-Q5

---
+++ Day 03/Q5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float celsius, fahrenheit;
+
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9.0 / 5.0) + 32;
+
+    printf("Temperature in Fahrenheit = %.2f°F\n", fahrenheit);
+}
